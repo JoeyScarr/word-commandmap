7dc58b146e15bb463bd90c11cba4a348e8bd0095
Joey Scarr <joey.scarr+github@gmail.com>
2013-08-22 18:03:05 +1200

    Add pre-computed task times (in seconds) to study harness logs

--- CMStudy1/Log.cs
+++ CMStudy1/Log.cs
@@ -7,93 +7,65 @@ using System.Drawing;
 using System.Windows.Forms;
 
 namespace CMStudy1 {
-  public static class Log {
-    private static bool loggingEnabled = true;
-    private static string filename = null;
-    private static string mouseFilename = null;
-    private static List<string> lines = new List<string>();
-    private static List<string> mouseLines = new List<string>();
-    private static int BUFFER_SIZE = 20;
+	public static class Log {
+		private static bool loggingEnabled = true;
+		private static string filename = null;
+		private static List<string> lines = new List<string>();
+		private static int BUFFER_SIZE = 20;
+		private static DateTime m_LastTaskStart;
 
-    public static void DisableLogging() {
-      loggingEnabled = false;
-    }
+		public static void DisableLogging() {
+			loggingEnabled = false;
+		}
 
-    public static void StartLogging(string filepath, string mouseFilePath = null) {
-      if (filename != null) {
-        Flush();
-      }
-      filename = filepath;
-      if (mouseFilename != null) {
-        FlushMouse();
-      }
-      mouseFilename = mouseFilePath;
-    }
+		public static void StartLogging(string filepath) {
+			if (filename != null) {
+				Flush();
+			}
+			filename = filepath;
+		}
 
-    private static void FlushIfNecessary() {
-      if (lines.Count >= BUFFER_SIZE) {
-        Flush();
-      }
-			if (mouseFilename != null && mouseLines.Count >= BUFFER_SIZE) {
-        FlushMouse();
-      }
-    }
+		private static void FlushIfNecessary() {
+			if (lines.Count >= BUFFER_SIZE) {
+				Flush();
+			}
+		}
 
-    public static void Flush() {
-      if (loggingEnabled) {
+		public static void Flush() {
+			if (loggingEnabled) {
 				Directory.CreateDirectory(Path.GetDirectoryName(filename));
-        using (StreamWriter sw = new StreamWriter(filename, true)) {
-          lock (lines) {
-            foreach (string line in lines) {
-              sw.WriteLine(line);
-            }
-            lines.Clear();
-          }
-        }
-      } else {
-        lines.Clear();
-      }
-    }
-
-    public static void FlushMouse() {
-      if (loggingEnabled) {
-        using (StreamWriter sw = new StreamWriter(mouseFilename, true)) {
-          lock (mouseLines) {
-            foreach (string line in mouseLines) {
-              sw.WriteLine(line);
-            }
-            mouseLines.Clear();
-          }
-        }
-      } else {
-        mouseLines.Clear();
-      }
-    }
+				lock (lines) {
+					using (StreamWriter sw = new StreamWriter(filename, true)) {
+						foreach (string line in lines) {
+							sw.WriteLine(line);
+						}
+						lines.Clear();
+					}
+				}
+			} else {
+				lines.Clear();
+			}
+		}
 
 		public static void LogTaskStart() {
-			LogString(string.Format("TASKSTART {0}", DateTime.Now.Ticks));
+			m_LastTaskStart = DateTime.Now;
+			LogString(string.Format("TASKSTART {0}", m_LastTaskStart.Ticks));
 		}
 
 		public static void LogTaskEnd() {
-			LogString(string.Format("TASKEND {0}", DateTime.Now.Ticks));
+			DateTime taskEnd = DateTime.Now;
+			LogString(string.Format("TASKEND {0} {1}", taskEnd.Ticks, (m_LastTaskStart - taskEnd).TotalSeconds));
 		}
 
 		public static void LogAppClosed() {
 			LogString(string.Format("APPCLOSED {0}", DateTime.Now.Ticks));
 		}
 
-    public static void LogString(string str) {
-      lock (lines) {
-        lines.Add(str);
-      }
-      FlushIfNecessary();
-    }
-
-    public static void LogMousePosition(Point pos) {
-      lock (mouseLines) {
-        mouseLines.Add(
-          string.Format("{0} {1} {2}", DateTime.Now.Ticks, pos.X, pos.Y));
-      }
-    }
-  }
+		public static void LogString(string str) {
+			lock (lines) {
+				lines.Add(str);
+			}
+			FlushIfNecessary();
+		}
+	}
 }
